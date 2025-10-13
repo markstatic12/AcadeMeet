73748a48bee407d416630b4ad2614d229c158bcd
rmcky0 <richemmae@outlook.com>
2025-10-13 17:13:23 +0800

    Setup(springboot): added springboot files

---
+++ backend/src/test/java/com/appdev/academeet/AcademeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.academeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
