37cce3e88a8a221af87f11ff1be9445bc19c7df8
LAB63_USER ON WS10 <L11Y08W10@LabHCI.net>
2025-11-20 08:09:01 +0800

    haha

--- frontend/src/components/notes/NotesGrid.jsx
+++ frontend/src/components/notes/NotesGrid.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import NoteCard from './NoteCard';
+import { useNavigate } from 'react-router-dom';
+import createNewCard from '../profile/CreateNewCard';
+import CreateNewCard from '../profile/CreateNewCard';
+
 
 const NotesGrid = ({ notes }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
+      
+      <CreateNewCard
+        onClick={() => navigate('/notes/new')}
+        label="Create New Note"
+      />  
+
       {notes.map((note) => (
         <NoteCard
           key={note.noteId || note.id}
