d1851258e09d41b9384046758bc167f32765032f
Zander <zander.aligato25@gmail.com>
2025-11-20 10:25:12 +0800

    fix(icon): home icon

--- frontend/src/icons/icons.jsx
+++ frontend/src/icons/icons.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 // Navigation Icons
 export const HomeIcon = ({ className }) => (
-  <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-3m0 0l7-4 7 4M5 9v10a1 1 0 001 1h12a1 1 0 001-1V9m-9 11l4-4m-9-8l7-4 7 4" />
+   <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
   </svg>
 );
 
